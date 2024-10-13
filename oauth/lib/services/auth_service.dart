af3b3fa4a3a5d008779368398b4ae31917df1d49
Thomas Steinert <hello@chroni.cc>
2024-10-13 19:01:23 +0200

    feat: add example for twitter login

--- oauth/lib/services/auth_service.dart
+++ oauth/lib/services/auth_service.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/foundation.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:oauth/secrets.dart';
 import 'package:sign_in_with_apple/sign_in_with_apple.dart';
+import 'package:twitter_login/twitter_login.dart';
 
 enum AuthProvider {
   apple,
@@ -12,6 +14,11 @@ enum AuthProvider {
 
 class AuthService extends ChangeNotifier {
   final GoogleSignIn _googleSignIn = GoogleSignIn();
+  final TwitterLogin _twitterLogin = TwitterLogin(
+    apiKey: xApiKey,
+    apiSecretKey: xApiSecretKey,
+    redirectURI: xApiRedirectUrl,
+  );
 
   AuthProvider? _provider;
 
@@ -26,6 +33,9 @@ class AuthService extends ChangeNotifier {
 
   Future<bool> signIn(AuthProvider provider) async {
     if (!_isSignedIn) {
+      // --------------------------------------------------------------------------------
+      // Apple
+      // --------------------------------------------------------------------------------
       switch (provider) {
         case AuthProvider.apple:
           try {
@@ -43,17 +53,39 @@ class AuthService extends ChangeNotifier {
             _displayName = '$account.givenName $account.familyName';
             _email = account.email;
           } catch (e) {
-            print(e);
+            print('Apple Error: $e');
             return false;
           }
           break;
+        // ------------------------------------------------------------------------------
+        // Google
+        // ------------------------------------------------------------------------------
         case AuthProvider.google:
           try {
             var account = await _googleSignIn.signIn();
             _displayName = account!.displayName;
             _email = account.email;
           } catch (e) {
-            print(e);
+            print('Google Error: $e');
+            return false;
+          }
+          break;
+        // ------------------------------------------------------------------------------
+        // X
+        // ------------------------------------------------------------------------------
+        case AuthProvider.x:
+          try {
+            debugPrint('X Login: ${_twitterLogin.toString()}.');
+            final account = await _twitterLogin.login();
+            debugPrint('X Account: $account');
+            debugPrint('X Account Status: ${account.status}');
+            debugPrint('X Account User: ${account.user}');
+            if (account.status == TwitterLoginStatus.loggedIn) {
+              _displayName = account.user!.name;
+              _email = account.user!.email;
+            }
+          } catch (e) {
+            debugPrint('X Error: $e');
             return false;
           }
           break;
@@ -69,8 +101,14 @@ class AuthService extends ChangeNotifier {
 
   Future<bool> signOut() async {
     switch (_provider) {
+      case AuthProvider.apple:
+        // Apple does not have a sign out method
+        break;
       case AuthProvider.google:
         await _googleSignIn.signOut();
+        break;
+      case AuthProvider.x:
+        // X does not have a sign out method
         break;
       default:
         return false;
--- oauth/macos/Flutter/GeneratedPluginRegistrant.swift
+++ oauth/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,8 +7,10 @@ import Foundation
 
 import google_sign_in_ios
 import sign_in_with_apple
+import twitter_login
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
   SignInWithApplePlugin.register(with: registry.registrar(forPlugin: "SignInWithApplePlugin"))
+  TwitterLoginPlugin.register(with: registry.registrar(forPlugin: "TwitterLoginPlugin"))
 }
